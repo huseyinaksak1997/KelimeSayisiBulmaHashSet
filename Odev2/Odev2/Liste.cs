45bddfbd747da9c067ba40c10c3a6deb1eaf4d7c
huseyinaksak1997 <huseyinaksak1997@gmail.com>
2023-06-11 18:29:56 +0300

    Add files via upload

---
+++ Odev2/Odev2/Liste.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odev2
+{
+    class Liste<T>:List<T>
+    {
+        string[] kelime = new string[100];
+        HashSet<string> hashset = new HashSet<string>();
+        public void HashEkle(string kelime)
+        {
+            string[] kelimeAr = kelime.Split(' ');
+            
+            foreach (var k in kelimeAr)
+            {
+                if (k != "")
+                {
+                    hashset.Add(k);
+                }
+                
+            }
+            
+        }
+        public int kelimeSayisi()
+        {
+            return hashset.Count();
+        }
+
+
+
+    }
+}
---
+++ Odev2/Odev2/Metin.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odev2
+{
+    public class Metin
+    {
+        public Metin()
+        {
+            Liste<string> liste = new Liste<string>();
+            Console.WriteLine("1.metini giriniz:");
+            liste.Add(Console.ReadLine().ToLower());
+
+            Console.WriteLine("2.metini giriniz:");
+            liste.Add(Console.ReadLine().ToLower());
+
+            Console.WriteLine("3.metini giriniz:");
+            liste.Add(Console.ReadLine().ToLower());
+            liste.HashEkle(liste[0]);
+            liste.HashEkle(liste[1]);
+            liste.HashEkle(liste[2]);
+            Console.WriteLine("Toplam girilen kelime sayısı:"+liste.kelimeSayisi());
+            Console.WriteLine("Farklı girilen kelime sayısı:"+(liste[0].Split(' ').Count()+ liste[1].Split(' ').Count()+ liste[2].Split(' ').Count()));
+
+           
+
+
+             
+        }
+
+
+
+
+    }
+}
